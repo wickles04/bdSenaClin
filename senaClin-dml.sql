6012e5a3d6722d9e3c9c222f4860d6f1b2d2d0a4
Wickles <wicklescorreia08@gmail.com>
2024-05-23 14:55:48 -0300

    dado inicio ao dml, adicionado 6 pacientes de forma implícita

---
+++ senaClin-dml.sql
@@ -0,0 +1,75 @@
+INSERT INTO paciente
+VALUES(
+	'1',
+	'Eliane Joana Mirella Aragão',
+	'01080616632',
+	'rua',
+	'Alameda Moema',
+	'122',
+	NULL,
+	'1345678954',
+	'santos');	
+INSERT INTO paciente
+VALUES(
+	'2',
+	'Luís Thiago Mendes',
+	'01035646632',
+	'rua',
+	'Rio Alalaú',
+	'52',
+	NULL,
+	'13155648625',
+	'santos');
+INSERT INTO paciente
+VALUES(
+	'3',
+	'André Benedito Fernandes',
+	'01087846982',
+	'avenida',
+	'Gláucia Maria dos Santos Gouveia',
+	'670',
+	NULL,
+	'13456496285',
+	'São Vicente');	
+INSERT INTO paciente
+VALUES(
+	'4',
+	'Arthur Nelson Martins',
+	'14290732763',
+	'estrada',
+	'Travessa Cirino de Almeida',
+	'5554',
+	NULL,
+	'13456494862',
+	'São Vicente');
+INSERT INTO paciente
+VALUES(
+	'5',
+	'Stefany Beatriz Gabriela Galvão',
+	'71301843709',
+	'avenida',
+	' Aporé',
+	'168',
+	NULL,
+	'13616871965',
+	'santos');
+INSERT INTO paciente
+VALUES(
+	'6',
+	'Sérgio Benício Sales',
+	'84193865029',
+	'estrada',
+	'Ipu',
+	'651',
+	NULL,
+	'13415496285',
+	'guarujá');
+	
+	
+	
+	
+
+	
+
+	
+	
